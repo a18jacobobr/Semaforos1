411ed91b2eac1ed61d3712071c03eafa30db28af
a18jacobobr <a18jacobobr@a13eq06.sanclemente.local>
2018-11-09 12:59:23 +0100

    fin de la clase del viernes

--- src/semaforos1/EntradaRecurso.java
+++ src/semaforos1/EntradaRecurso.java
@@ -1,5 +1,28 @@
 package semaforos1;
 
+import java.util.concurrent.Semaphore;
+
 public class EntradaRecurso {
+	
+	Semaphore semaforo = new Semaphore(3);
+	
+	public void bloquear () {
+		
+		try {
+			semaforo.acquire();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Estoy dentro");
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		semaforo.release();
+		System.out.println("Libero");
+	}
+	
+	
 
 }
---
+++ src/semaforos1/Programa.java
@@ -0,0 +1,12 @@
+package semaforos1;
+
+public class Programa extends Thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntradaRecurso entrada = new EntradaRecurso();
+		
+
+	}
+
+}
